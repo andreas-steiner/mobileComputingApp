a0416e228a58c58ca4d1d1af3bee6a897931d3c6
andreas-steiner <andreas.steiner115@gmail.com>
2023-02-11 13:50:06 +0100

    Projektdateien hinzufügen.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Shared;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		var x = new Class1();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		lblmaui.Text = "Hallo";
+
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
