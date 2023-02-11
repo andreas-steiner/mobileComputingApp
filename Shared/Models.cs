bdf0e610ef5135173e468c4830d45463ce0ce4d3
BernhardNinaus <ninausbernhard@gmail.com>
2023-02-11 20:33:09 +0100

    changed shard Class1 to Models

---
+++ Shared/Models.cs
@@ -0,0 +1,40 @@
+﻿namespace Shared;
+
+public class Species {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public int Speed { get; set; }
+	public List<AbilityScore> AbilityScores { get; set; } = new();
+	public string Alignement { get; set; } = string.Empty;
+	public int Age { get; set; }
+	public string Size { get; set; } = string.Empty;
+	public List<StartingProficency> StartingProficencies { get; set; } = new();
+	public List<Lang> Langs { get; set; } = new();
+	public List<Trait> Traits { get; set; } = new();
+	public List<SubRace> SubRaces { get; set; } = new();
+}
+
+public class AbilityScore {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
+
+public class StartingProficency {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
+
+public class Lang {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
+
+public class Trait {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
+
+public class SubRace {
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
